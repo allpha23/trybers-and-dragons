40ecf04e1e05543516c5c1fe617b0a3881ef595b
allpha23 <allpha23@gmail.com>
2022-07-11 03:05:41 -0300

    Requisito 2 finalizado

---
+++ src/Races/Dwarf.ts
@@ -0,0 +1,20 @@
+import Race from './Race';
+
+class Dwarf extends Race {
+  maxLifePoints: number;
+
+  static numRaces = 0;
+
+  constructor(name: string, dexterity: number) {
+    super(name, dexterity);
+
+    this.maxLifePoints = 80;
+    Dwarf.numRaces += 1; 
+  }
+
+  static createdRacesInstances(): number {
+    return Dwarf.numRaces;
+  }
+}
+
+export default Dwarf;
---
+++ src/Races/Elf.ts
@@ -0,0 +1,20 @@
+import Race from './Race';
+
+class Elf extends Race {
+  maxLifePoints: number;
+
+  static numRaces = 0;
+
+  constructor(name: string, dexterity: number) {
+    super(name, dexterity);
+
+    this.maxLifePoints = 99;
+    Elf.numRaces += 1; 
+  }
+
+  static createdRacesInstances(): number {
+    return Elf.numRaces;
+  }
+}
+
+export default Elf;
